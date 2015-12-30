8a905aee8ac07161ee1c50136b8bf6d3aa598740
Todd Wolfson <todd@twolfson.com>
2015-12-29 21:01:09 -0600

    Found a place where we match our command

--- bin/google-music-electron.js
+++ bin/google-music-electron.js
@@ -6,15 +6,18 @@ var electronPath = require('electron-prebuilt');
 var parseCli = require('../lib/cli-parser').parse;
 
 // Process our arguments (catches any `--help` and `install-mpris` commands)
-parseCli(process.argv);
+var program = parseCli(process.argv);
 
-// Find our application
-var googleMusicElectronPath = path.join(__dirname, '..');
-var args = [googleMusicElectronPath];
+// If didn't match a command (e.g. `install-mpris`), then launch our application
+if (program.args.length === 0) {
+  // Find our application
+  var googleMusicElectronPath = path.join(__dirname, '..');
+  var args = [googleMusicElectronPath];
 
-// Append all arguments after our node invocation
-// e.g. `node bin/google-music-electron.js --version` -> `--version`
-args = args.concat(process.argv.slice(2));
+  // Append all arguments after our node invocation
+  // e.g. `node bin/google-music-electron.js --version` -> `--version`
+  args = args.concat(process.argv.slice(2));
 
-// Run electron on our application and forward all stdio
-spawn(electronPath, args, {stdio: [0, 1, 2]});
+  // Run electron on our application and forward all stdio
+  spawn(electronPath, args, {stdio: [0, 1, 2]});
+}
