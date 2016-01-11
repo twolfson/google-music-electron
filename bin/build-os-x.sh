86bfe0261a0089d28a9258b9938854a2408abcb9
Todd Wolfson <todd@twolfson.com>
2016-01-10 20:34:03 -0600

    Starting on OS X build script

---
+++ bin/build-os-x.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Exit on our first error and echo commands
+set -e
+set -x
+
+# Copy our library and `package.json` into a build directory
+if test -d tmp-build; then
+  rm -r tmp-build
+fi
+mkdir tmp-build
+cp -r lib tmp-build
+cp package.json tmp-build
+
+# Navigate into our build directory
+cd tmp-build
+
+# Install our production only dependencies
+npm install --production
+
+# Navigate back to our development directory
+cd -
+
+# Compile our OS X application
+# TODO: Figure out command here
